5da6011d556e75d216c8e32e388e92c0bf0f6c80
Swaraj0769 <swarajsanodiya6@gmail.com>
2025-07-31 18:59:49 +0530

    server is connected to socket.io

--- real-time-tracker/app.js
+++ real-time-tracker/app.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const http = require('http')
 
 const app= express()
+
+const socketio = require('socket.io')
+const server = http.createServer(app)
+const io = socketio(server)
 
 app.get('/', function(req, res){
     res.send("hey")
 })
 
-app.listen(3000)
+server.listen(3000)
